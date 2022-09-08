30c0ca1ca54520dccab5c05706ad6ed5fcd2e472
Jonjo7Dia <jonosphgames@gmail.com>
2022-09-08 16:08:57 -0300

    adding images

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function FirstPost() {
-    return <h1>First Post</h1>;
-  }
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+      <Image
+        src="/images/profile.png" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
+    </>
+  );
+}
