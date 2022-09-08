50497d058800d216f07bbb14731450f93bd98621
Jonjo7Dia <jonosphgames@gmail.com>
2022-09-08 19:33:35 -0300

    layout updated

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
+import Image from "next/image";
+
 import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link';
-import Date from '../components/date';
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
@@ -13,24 +15,29 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
+        <Image
+          priority
+          src="/images/profile.png"
+          className={utilStyles.borderCircle}
+          height={144}
+          width={144}
+          alt={"jonathan profile pic"}
+        />
         <p>
           Hello I am <strong>Jonathan</strong> I am a software developer from
           Denmark. I love learning about new technologies and coding! You can
-          contact me here {" "}
+          contact me here{" "}
           <a href="https://www.linkedin.com/in/svend-petter-jonathan-hjelmstrom-573882168/">
-             Linkedin
-          </a>.
-        </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+            Linkedin
+          </a>
+          .
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -38,14 +45,14 @@ export default function Home({allPostsData}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
         </ul>
       </section>
