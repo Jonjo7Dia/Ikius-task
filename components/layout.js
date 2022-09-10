eed4c51df1f51e19f550c7d61a3fc746d4674556
Jonjo7Dia <jonosphgames@gmail.com>
2022-09-10 17:07:19 -0300

    layout refactored

--- components/layout.js
+++ components/layout.js
@@ -1,28 +1,11 @@
-import Head from "next/head";
 import styles from "./layout.module.css";
 import Link from "next/link";
 
-const name = "Jonathan Hjelmstrom's Ikius test case";
-export const siteTitle = "Ikius test case";
 
 export default function Layout({ children, links}) {
   return (
     <div className={styles.layout}>
       <div className={styles.container}>
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="Learn how to build a personal website using Next.js"
-          />
-          <meta
-            property="og:image"
-            content={`https://og-image.vercel.app/${encodeURI(
-              siteTitle
-            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-          />
-          <meta name={name} content={siteTitle} />
-        </Head>
         <main>{children}</main>
       </div>
       <footer className={styles.footer}>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
+import Layout from "../components/layout";
 import {Image, StructuredText, renderMetaTags } from "react-datocms";
 
 import utilStyles from "../styles/utils.module.css";
